efa4eb508488f01c60f9cbd0868da8ba96ff4d70
GURUPRASAD H D <gurudilp456@gmail.com>
2025-08-15 21:53:02 +0530

    admin panel

--- wealthmanager/wealthmanager/urls.py
+++ wealthmanager/wealthmanager/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('grappelli/', include('grappelli.urls')),  # before admin
     path('admin/', admin.site.urls),
     path('api/', include('portfolio.urls')),
 ]
